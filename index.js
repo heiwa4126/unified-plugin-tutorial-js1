d0b18272419b977585e71f8225eaeb80c37b0754
heiwa4126 <heiwa4126@gmail.com>
2023-04-25 11:28:35 +0900

    https://unifiedjs.com/learn/guide/using-unified/#plugins

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import process from "node:process";
+import rehypeDocument from "rehype-document";
 import rehypeStringify from "rehype-stringify";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
+import remarkSlug from "remark-slug";
+import remarkToc from "remark-toc";
 import { unified } from "unified";
 import { stream } from "unified-stream";
 
-const processor = unified().use(remarkParse).use(remarkRehype).use(rehypeStringify);
+// const processor = unified().use(remarkParse).use(remarkRehype).use(rehypeStringify);
+
+const processor = unified().use(remarkParse).use(remarkSlug).use(remarkToc).use(remarkRehype).use(rehypeDocument, { title: "Contents" }).use(rehypeStringify);
 
 process.stdin.pipe(stream(processor)).pipe(process.stdout);
